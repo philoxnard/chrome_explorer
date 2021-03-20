a51e1a1027879fd160e09f4b5fd7ca2f85ebbb73
philoxnard <phil.oxnard@gmail.com>
2021-03-20 11:51:14 -0400

    Initial commit. Put in the general structure of the game and a small initial README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # chrome_explorer
+
+Chrome Explorer (the name is WIP) is an in-development online game that will take place in a Chrome extension. The back-end will be in Python utilizing Flask to package it as a web app, and the front-end will utilize HTML, CSS, Javascript, and potentially Socket.IO if the need arises.
+
+Chrome Explorer is a game that exists somewhere between Pokemon, Pokemon-GO, and Skannerz. It is a Pokemon-style game that uses the internet as its game map. As players traverse the web and hop from site to site, they will encounter different monsters (the name of which is to be determined - cybermon, webbies, phoxmon, etc). They can then use their own monsters to battle the ones they encounter in the wild, strenghtening their own monsters while also adding to their collection each time they find a new one.
+
+## General Structure
+
+When a user turns on the Chrome Explorer extension, it will start pinging the Chrome Explorer server every time a new web address is visited (probably only listening on one tab to avoid confusion/errors). The server will take the URL and identify the domain name, which it will then check against a pre-made list of "Regions" such as Social Media, Video Streaming, News, Search Engine, etc. Much like how different regions of Pokemon have different Pokemon living in them, so too will the different Regions have different monsters lurking as potential encounters.
+
+When the server decides that an encounter with a wild monster will happen, the user will be given the option to engage it or run away. If the user engages, they will find themselves in a Pokemon-style battle (with slight tweaks to mechanics as I see fit). Defeating wild monsters will give your active monster more experience so they can level up, get stronger, and learn new attacks. It will also add any newly encountered monsters to the user's collection.
---
+++ main.py
@@ -0,0 +1,47 @@
+# Import game
+
+if __name__ == "__main__":
+
+    #instantiate Game
+
+    while True:
+        if game.state == "explore":
+            url = input("Take the URL from each new page and send it here")
+            # Server will take the URL and grab the domain name
+            # Server will take domain name and return a region
+            # Server will run calcs on what mons are in the region
+            # Server will determine if an encounter happens or not, and with what mon
+            # If encounter happens:
+            run_or_engage = input("The player will click Run or Engage")
+            # Server will determine based on player input:
+                # If "Skip", then nothing happens
+                # If "Engage", then set game.state to "initialze_encounter"
+        if game.state == "initialize_encounter":
+            # Server will tell browser to display a pokemon-type battle screen
+            # Server will populate active_mon and active_enemy_mon
+            # Server will set game.state to "encounter"
+        if game.state == "encounter":
+            # Server will run calcs to determine whose turn it is - add up speed from each mon until someone hits 100
+            # If enemy's turn, server will pick an attack and execute
+            # If player's turn, server will tell browser what options player has
+            encounter_action = input("Player will Attack, Run, or Swap")
+            # Server will take input and decide what to do based on it
+                # If run, server will give browser an end-the-fight message and set game.state to "explore"
+                # If swap, server will give browser a menu of who to swap to based on user collection
+                # If attack, server will take the attack and run calcs to assign damage/status effects
+            # If player mon dies, force swap or run
+            # If enemy mon dies, set game.state to "encounter_cleanup"
+        if game.state == "encounter_cleanup":
+            # Server will calculate experience gain for the active mon
+            # Server will add enemy mon to user's collection
+            # Server will set game.state to "explore"
+
+# This is just the base framework for the game.
+# This file will not actually be used in the actual release version of the game
+# This file is only meant for planning purposes, and potentially for play testing
+# here in the Python terminal before the actual front-end is set up or connected
+# As it gets developed, new features will be added such as:
+    # Buttons in browser to naviage through a user's party and a user's collection
+    # Option for players to battle each other
+    # Some kind of general story, or at least a kind of gym battle system
+
