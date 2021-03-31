626d44d6731a6b32b076d8841b091ce17560b1d2
philoxnard <phil.oxnard@gmail.com>
2021-03-31 16:40:23 -0400

    Successfully passing information from front end to back end via different routes - HUGE accomplishment

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, make_response
+from flask_cors import CORS
+
 
 from game import Game
+
 game = Game()
+
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 
-@app.route('/')
-def sessions():
-    text = game.regions
-    x = text.find({"region": "streaming"})
-    for doc in x:
-        return doc["domains"][0]
+@app.route('/login', methods=["GET", "POST"])
+def handle_login():
+    if request.method == "POST":
+        username = request.form["username"]
+        print(username)
+        return 'Success', 200
+
+@app.route('/test', methods=["POST"])
+def test_func():
+    data = request.form["data"]
+    print(f"Does this work? {data}")
+    return 'Success', 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
--- extension/static/popup.js
+++ extension/static/popup.js
@@ -1,10 +1,15 @@
-const form = document.getElementById('signupLink')
-const log = document.getElementById('log')
-
-function submit() {
-    console.log('founasdfd')
-}
+const form = document.getElementById('getPlayer')
 
 if (form) {
-    form.addEventListener('click', submit)
+    form.addEventListener('submit', function(){
+        const req = new XMLHttpRequest()
+        const baseUrl = "http://127.0.0.1:5000"
+        const username = document.getElementById("username").value
+        $.post(baseUrl+"/login", {
+            username: username
+        })
+        $.post(baseUrl+"/test", {
+            data: "does this work"
+        })
+    })
 }
