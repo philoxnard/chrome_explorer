bf4cdcd9ec51d1d7da7ff757cfdebb72baf342c5
philoxnard <phil.oxnard@gmail.com>
2021-04-14 12:50:35 -0400

    Desktop notification now properly appears when you find a phoxgit add extension/background.js extension/manifest.json  This is awesome!

--- extension/background.js
+++ extension/background.js
@@ -12,7 +12,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
                 console.log(data)
                 // Manifest V3 apparently doesnt work for notifications, so
                 // this will eventually get implemented as written
-                /////////////////////////////////////////////////////////////
+                // Currently works as a desktop notification, which is fine
+                // But I'm going to copy/paste here the chrome notification syntax
+                ////////////////////////////////////////////////////////////
                 // if (data.state == "initialize encounter"){
                 //     console.log("You did it")
                 //     chrome.notifications.create('', {
@@ -22,18 +24,25 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
                 //         type: 'basic'
                 //     })
                 // }
+                /////////////////////////////////////////////////////////////
+                if (data.state == "initialize encounter"){
+                    console.log("You did it")
+                    registration.showNotification('Phoxtrot', {
+                        body: "You found a phox! Click the extention to fight it!"
+                    })
+                }
             })
         })
     }
 })
 
 
-chrome.tabs.onRemoved.addListener(function(tabid, removed){
-    fetch('http://127.0.0.1:5000/exit')
-    .then(function() {
-        console.log('Closing response detected')
-    })
-})
+// chrome.tabs.onRemoved.addListener(function(tabid, removed){
+//     fetch('http://127.0.0.1:5000/exit')
+//     .then(function() {
+//         console.log('Closing response detected')
+//     })
+// })
 
 addEventListener('unload', function(){
     fetch('http://127.0.0.1:5000/exit')
